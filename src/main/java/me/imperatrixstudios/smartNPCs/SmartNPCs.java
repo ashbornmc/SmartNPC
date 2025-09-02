e5b9362f382290241ef6ac07586d8ebf509003fa
ashbornmc <ceo@ashbornmc.eu>
2025-09-03 01:28:58 +0300

    default

---
+++ src/main/java/me/imperatrixstudios/smartNPCs/SmartNPCs.java
@@ -0,0 +1,17 @@
+package me.imperatrixstudios.smartNPCs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmartNPCs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
